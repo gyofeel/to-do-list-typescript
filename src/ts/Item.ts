71076fe5a00648d6f3f2309ce0efe70fdfae5c75
서교필 <gyofeel318@gmail.com>
2020-10-11 20:43:06 +0900

    feat: add Item class and several types
    - add types(Priority, Item)

--- src/ts/Item.ts
+++ src/ts/Item.ts
@@ -1,3 +1,30 @@
-class Item {
+import { Priority, ItemValue } from './config/types';
 
+export class Item {
+    public constructor({ content, priority}:ItemValue) {
+        this.content = content;
+        this.priority = priority;
+    }
+
+    private content:string = '';
+    private priority:Priority = 'medium';
+    private isDone:boolean = false;
+    private date:Date = new Date();
+    private id:string = `id-${this.date}`;
+
+    setContent(content:string) {
+        this.content = content;
+    }
+    setPriority(priority:Priority) {
+        this.priority = priority;
+    }
+    setDone(done:boolean) {
+        this.isDone = done;
+    }
+    getDate() {
+        return this.date;
+    }
+    getId() {
+        return this.id;
+    }
 }
---
+++ src/ts/config/types.ts
@@ -0,0 +1,7 @@
+// Item
+export type Priority = 'low' | 'medium' | 'high';
+
+export type ItemValue = {
+    content: string,
+    priority: Priority
+};
